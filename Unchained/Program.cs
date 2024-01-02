62e7a1bf669659eecda4562efc4a84ecd75b2737
William Pescherine <wkpescherine@gmail.com>
2024-01-01 22:02:59 -0500

    Unchained:    2.2.1:       Added some basic colors elements       Added Main Screen panel

--- Unchained/Program.cs
+++ Unchained/Program.cs
@@ -9,23 +9,32 @@ namespace unchained{
     public static class Program{
         public static void Main(String [] args){
             Form window = new Form();
+            Panel main = new Panel();
             Label text = new Label();
             Button button = new Button();
 
             button.Text = "Enter";
-            button.Location = new Point(400, 150);
-            button.Click += new EventHandler(testClick);
+            button.Location = new Point(395, 150);
+            button.BackColor = Color.FromArgb(255, 255, 255);
+            button.Click += testClick;
             
             text.Text = "Unchained";
             //The location is (Width,hieght)
             text.Location = new Point(400,100);
+            text.ForeColor = Color.FromArgb(255, 255, 255);
+
+            main.Width = 100;
+            main.Height = 100;
+            main.BackColor = Color.FromArgb(255, 0, 0);
             
             window.Width = 800;
             window.Height = 600;
-            window.Text = "Unchained v2.1.3";
+            window.Text = "Unchained v2.2.1";
+            window.BackColor = Color.FromArgb(0, 0, 0);
 
             window.Controls.Add(text);
             window.Controls.Add(button);
+            window.Controls.Add(main);
             Application.Run(window);
         }
 
