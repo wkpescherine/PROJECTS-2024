45e7927093bf1bddf1a9f466e4e87a647a3bf5f8
William Pescherine <wkpescherine@yahoo.com>
2024-01-01 22:30:07 -0500

    Unchained:    2.2.2:       Added char selection screen       Starting to build out the logic of screen display

--- Unchained/Program.cs
+++ Unchained/Program.cs
@@ -10,7 +10,7 @@ namespace unchained{
         public static void Main(String [] args){
             Form window = new Form();
             Panel main = new Panel();
-            Panel char = new Panel();
+            Panel select = new Panel();
             Label text = new Label();
             Button button = new Button();
 
@@ -24,18 +24,25 @@ namespace unchained{
             text.Location = new Point(400,100);
             text.ForeColor = Color.FromArgb(255, 255, 255);
 
+            select.Width = 100;
+            select.Height = 100;
+            select.Visible = false;
+            select.BackColor = Color.FromArgb(0,255,0);
+
             main.Width = 100;
             main.Height = 100;
-            main.BackColor = Color.FromArgb(255, 0, 0);
+            main.Visible = false;
+            main.BackColor = Color.FromArgb(0, 0, 0);
             
             window.Width = 800;
             window.Height = 600;
-            window.Text = "Unchained v2.2.1";
+            window.Text = "Unchained v2.2.2";
             window.BackColor = Color.FromArgb(0, 0, 0);
 
             window.Controls.Add(text);
             window.Controls.Add(button);
             window.Controls.Add(main);
+            window.Controls.Add(select);
             Application.Run(window);
         }
 
