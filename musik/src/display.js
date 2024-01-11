a186969b1e2d2a6cbc6ecc399de3cb70c3b88bc1
William Pescherine <wkpescherine@yahoo.com>
2024-01-10 23:15:53 -0500

    musik:      0.5.1:           Added Side bar panel to UTS           Added Display Area to UTS           Added Categories to sidebar

---
+++ musik/src/display.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function Display (){
+    return (
+        <div>
+            <p>Display area under development</p> 
+            <button>Does Nothing</button>
+        </div>  
+    );
+}
--- musik/src/uts.js
+++ musik/src/uts.js
@@ -1,9 +1,16 @@
 import './App.css';
 
+import SideBar from "./sidebar"
+import Display from "./display"
+
 export default function UTS (){
     return (
         <div>
-            <p>UTS under development</p> 
+            <p>UTS under development</p>
+            <div style={{display: "flex",justifyContent: "center"}}>
+                <SideBar />
+                <Display /> 
+            </div>
         </div>  
     );
 }
