82de78726382952b6b646929e634c3fab7c1b520
William Pescherine <wkpescherine@yahoo.com>
2024-01-25 21:42:41 -0500

    Unchained:         0.2.8.2:             Added more options to character creation screen

--- Unchained/Program.cs
+++ Unchained/Program.cs
@@ -33,56 +33,116 @@ namespace unchained{
             selectExit.BackColor = Color.FromArgb(255, 255, 255);
             selectExit.Click += backToMain;
 
-            
+            //Style handling section
+            Label styleHeader = new();
+            styleHeader.Text = "Select your Style";
+            styleHeader.ForeColor = Color.FromArgb(255, 255, 255);
+            styleHeader.Location = new Point(25,25);
+            Panel spacer1 = new();
+            spacer1.Width = 350;
+            spacer1.Height = 3;
+            spacer1.Location = new Point(25, 55);
+            spacer1.BackColor = Color.FromArgb(255, 255, 255);
             Button holy = new();
             holy.Text = "Holy";
-            holy.Location = new Point(25, 25);
+            holy.Location = new Point(25, 80);
             holy.BackColor = Color.FromArgb(255, 255, 255);
-            Button stout = new();
-            stout.Text = "Stout";
-            stout.Location = new Point(125, 25);
-            stout.BackColor = Color.FromArgb(255, 255, 255);
             Button wise = new();
             wise.Text = "Wise";
-            wise.Location = new Point(25, 75);
+            wise.Location = new Point(25, 110);
             wise.BackColor = Color.FromArgb(255, 255, 255);
+            Button psychtic = new();
+            psychtic.Text = "Psychtic";
+            psychtic.Location = new Point(25, 140);
+            psychtic.BackColor = Color.FromArgb(255, 255, 255);
+            Button stout = new();
+            stout.Text = "Stout";
+            stout.Location = new Point(125, 80);
+            stout.BackColor = Color.FromArgb(255, 255, 255);
+            Button veteran = new();
+            veteran.Text = "Veteran";
+            veteran.Location = new Point(125, 110);
+            veteran.BackColor = Color.FromArgb(255, 255, 255);
+            Button brutal = new();
+            brutal.Text = "Brutal";
+            brutal.Location = new Point(125, 140);
+            brutal.BackColor = Color.FromArgb(255, 255, 255);
             Button nimble = new();
             nimble.Text = "Nimble";
-            nimble.Location = new Point(225, 25);
+            nimble.Location = new Point(225, 80);
             nimble.BackColor = Color.FromArgb(255, 255, 255);
+            Button balanced = new();
+            balanced.Text = "Balanced";
+            balanced.Location = new Point(225, 110);
+            balanced.BackColor = Color.FromArgb(255, 255, 255);
+            Button speedy = new();
+            speedy.Text = "Speedy";
+            speedy.Location = new Point(225, 140);
+            speedy.BackColor = Color.FromArgb(255, 255, 255);
+            //Race handling section
+            Label raceHeader = new();
+            raceHeader.Text = "Select your Race";
+            raceHeader.ForeColor = Color.FromArgb(255, 255, 255);
+            raceHeader.Location = new Point(25,190);
+            Panel spacer2 = new();
+            spacer2.Width = 350;
+            spacer2.Height = 3;
+            spacer2.Location = new Point(25, 220);
+            spacer2.BackColor = Color.FromArgb(255, 255, 255);
             Button human = new();
             human.Text = "Human";
-            human.Location = new Point(25, 125);
+            human.Location = new Point(25, 245);
             human.BackColor = Color.FromArgb(255, 255, 255);
             Button dwarf = new();
             dwarf.Text = "Dwarven";
-            dwarf.Location = new Point(125, 125);
+            dwarf.Location = new Point(125, 245);
             dwarf.BackColor = Color.FromArgb(255, 255, 255);
+            //Role handling section
             Button paladin = new();
             paladin.Text = "Paladin";
-            paladin.Location = new Point(25, 225);
+            paladin.Location = new Point(25, 420);
             paladin.BackColor = Color.FromArgb(255, 255, 255);
+            //Results section
             Label selectStyle = new();
             selectStyle.Text = charSheet.style;
-            selectStyle.Location = new Point(25,325);
+            selectStyle.Location = new Point(25, 520);
             selectStyle.ForeColor = Color.FromArgb(255, 255, 255);
             Label selectRace = new();
             selectRace.Text = charSheet.style;
-            selectRace.Location = new Point(125,325);
+            selectRace.Location = new Point(125, 520);
             selectRace.ForeColor = Color.FromArgb(255, 255, 255);
+            //Event handling section
             holy.Click += (sender, e) => styleSelect(sender, e);
-            stout.Click += (sender, e) => styleSelect(sender, e);
             wise.Click += (sender, e) => styleSelect(sender, e);
+            psychtic.Click += (sender, e) => styleSelect(sender, e);
+            stout.Click += (sender, e) => styleSelect(sender, e);
+            veteran.Click += (sender, e) => styleSelect(sender, e);
+            brutal.Click += (sender, e) => styleSelect(sender, e);
             nimble.Click += (Sender, e) => styleSelect(Sender,e);
+            balanced.Click += (Sender, e) => styleSelect(Sender,e);
+            speedy.Click += (Sender, e) => styleSelect(Sender,e);
+            balanced.Click += (Sender, e) => styleSelect(Sender,e);
+            speedy.Click += (Sender, e) => styleSelect(Sender,e);
             human.Click += (Sender, e) => raceSelect(Sender,e);
             dwarf.Click += (Sender, e) => raceSelect(Sender,e);
+            paladin.Click += (Sender, e) => roleSelect(Sender,e);
+            //UI section
             select.Width = 800;
             select.Height = 600;
             select.Visible = false;
+            select.Controls.Add(styleHeader);
+            select.Controls.Add(spacer1);
             select.Controls.Add(holy);
-            select.Controls.Add(stout);
             select.Controls.Add(wise);
+            select.Controls.Add(psychtic);
+            select.Controls.Add(stout);
+            select.Controls.Add(veteran);
+            select.Controls.Add(brutal);
             select.Controls.Add(nimble);
+            select.Controls.Add(balanced);
+            select.Controls.Add(speedy);
+            select.Controls.Add(raceHeader);
+            select.Controls.Add(spacer2);            
             select.Controls.Add(human);
             select.Controls.Add(dwarf);
             select.Controls.Add(paladin);
@@ -122,7 +182,7 @@ namespace unchained{
             
             window.Width = 800;
             window.Height = 600;
-            window.Text = "Unchained v2.7.3";
+            window.Text = "Unchained v0.2.8.2";
             //window.BackColor = Color.FromArgb(0, 0, 0);
             //window.Controls.Add(button);
             window.Controls.Add(main);
@@ -138,6 +198,13 @@ namespace unchained{
             }
 
             void raceSelect(object ? sender, EventArgs e){
+                Button btn2 = sender as Button;
+                string r = btn2.Text;
+                charSheet.race = r;
+                selectRace.Text = charSheet.race;
+            }
+            
+            void roleSelect(object ? sender, EventArgs e){
                 Button btn2 = sender as Button;
                 string r = btn2.Text;
                 charSheet.race = r;
