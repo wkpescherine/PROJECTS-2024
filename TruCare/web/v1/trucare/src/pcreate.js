242da80acaed05ab09ace8ea4b3dd26c8f33e6a8
William Pescherine <wkpescherine@yahoo.com>
2024-02-18 23:37:29 -0500

    TruCare         0.6             Update to Patient create screen             Added doctors, states and pay rate to Patient screen             Added more Patient info sections

--- TruCare/web/v1/trucare/src/pcreate.js
+++ TruCare/web/v1/trucare/src/pcreate.js
@@ -33,10 +33,39 @@ function PCreate() {
             <div>
                 <label>Username</label> 
                 <input type="text" placeholder="Enter username" onChange={handleLogInUsernameChange}></input>
-            </div>
-            <div>
                 <label>Password</label>
                 <input type="password" placeholder="Enter password" onChange={handleLogInPasswordChange}></input>
+            </div>
+            <div>
+                <label>Email</label>
+                <input type="password" placeholder="Enter password" onChange={handleLogInPasswordChange}></input>
+                <label>Age: Yrs</label>
+                <input type="text" placeholder="YRS" class="inputData"></input>
+                <label>Mos</label>
+                <input type="text" placeholder="MOS" class="inputData"></input>
+            </div>
+            <div>
+                <p style={{margin:"5px"}}>Select Doctor:</p>
+                <hr style={{background:"#fff", height: "1px", width: "500px"}}/>
+                <div>
+                    <button>Family</button>
+                    <button>Dentist</button>
+                    <button>Eye</button>
+                </div>
+                <hr style={{background:"#fff", height: "1px", width: "500px"}}/>
+                <p style={{margin:"5px"}}>State:</p>
+                <hr style={{background:"#fff", height: "1px", width: "500px"}}/>
+                <hr style={{background:"#fff", height: "1px", width: "500px"}}/>
+                <p style={{margin:"5px"}}>Rate of Coverage:</p>
+                <hr style={{background:"#fff", height: "1px", width: "500px"}}/>
+                <div>
+                    <label>
+                        <input type="radio" value="normal"/>Slow
+                        <input type="radio" value="normal"/>Normal
+                        <input type="radio" value="normal"/>Fast
+                    </label>
+                </div>
+                <hr style={{background:"#fff", height: "1px", width: "500px"}}/>                
             </div>
             <p>
                 <button class='invertButton' onClick={RedirectToExit}>Exit</button> 
