2deec160c61adc990a8933d1ce2b0c120a5ab373
William Pescherine <wkpescherine@yahoo.com>
2024-02-27 23:20:23 -0500

    Unchained:         0.2.19:             Added Key pressing to Dungeon (WIP)             Updated Dungeon screen to now appear             Updated Program file by cleaning up unneeded code

--- Unchained/Program.cs
+++ Unchained/Program.cs
@@ -6,10 +6,6 @@ using System.CodeDom;
 using System.Drawing.Text;
 using System.Windows.Media.Imaging;
 
-//Most of the following code works and does produce the window
-//Having to work on converting from the Java coud that works to C#
-//This is now working and continueing to build it up.
-
 namespace unchained{
     public static class Program{
         public static void Main(String [] args){
@@ -29,37 +25,15 @@ namespace unchained{
             ScreenManager.pan4 = dungeonScreen;
             ScreenManager.HandleScreenChanges(ScreenManager.pan1, ScreenManager.pan2, ScreenManager.pan3, ScreenManager.pan4);
             
-            //selectExit.Click += backToMain;
-
-            Button startGame = new();
-            startGame.Text = "Start Game";
-            startGame.Location = new Point(450, 525);
-            startGame.BackColor = Color.FromArgb(255, 255, 255);
-            //startGame.Click += startNewGame;
-            
             window.Width = 800;
             window.Height = 600;
-            window.Text = "Unchained v0.2.18";
+            window.Text = "Unchained v0.2.19";
             window.Controls.Add(mainScreen);
             window.Controls.Add(savedScreen);
             window.Controls.Add(selectionScreen);
+            window.Controls.Add(dungeonScreen);
             window.Update();
             Application.Run(window);
-
-            
-            //void backToMain(Object ? sender, EventArgs e){
-                //main.Visible = true;
-                //select.Visible = false;
-                //savedScreen.Visible = false;
-            //}
-
-            //void startNewGame(Object ? sender, EventArgs e){
-            //    select.Visible = false;
-                //dungeon.Visible = true;
-            //    Dungeon dungeon = new Dungeon();
-            //    Panel game = dungeon.DungeonPanel();
-            //    window.Controls.Add(game);
-            //}
         }
     }
 }
--- Unchained/dungeon.cs
+++ Unchained/dungeon.cs
@@ -10,16 +10,30 @@ namespace unchained{
     public class Dungeon{ 
         public Panel DungeonPanel(){
             Panel gamePanel = new();
+            
             Button quitGame = new();
+            quitGame.BackColor = Color.FromArgb( 255, 255 , 255);
+            quitGame.Width = 200;
+            quitGame.Location = new System.Drawing.Point(395, 150);
+            quitGame.Text = "Quit Game";
             quitGame.Click += quitGamePlay;
 
             gamePanel.Width = 800;
             gamePanel.Height = 600;
+            gamePanel.BackColor = Color.FromArgb(0, 0, 0);
             gamePanel.Visible = false;
+            //(Sender, e) => newGame(Sender,e);
+            gamePanel.KeyPress += (Sender, e) =>detectKeyPress(Sender, e);
             gamePanel.Controls.Add(quitGame); 
 
             void quitGamePlay(Object ? sender, EventArgs e){
                 ScreenManager.screen = "Main";
+                ScreenManager.HandleScreenChanges(ScreenManager.pan1, ScreenManager.pan2, ScreenManager.pan3, ScreenManager.pan4);
+            }
+
+            void detectKeyPress(Object ? sender, KeyPressEventArgs e){
+                Console.Write("1");
+                if(e.KeyChar == (char)Keys.F){Console.Write("Test");}
             }
 
             return gamePanel;
