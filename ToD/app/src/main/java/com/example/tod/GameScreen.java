50434712e8035ed3b122571e7426f5b8e9247297
William Pescherine <wkpescherine@yahoo.com>
2024-03-14 22:59:36 -0400

    ToD:         0.1:             Initial Setup             Added Main Activity             Added Game Screen Activity

---
+++ ToD/app/src/main/java/com/example/tod/GameScreen.java
@@ -0,0 +1,18 @@
+package com.example.tod;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class GameScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getSupportActionBar().hide();
+    }
+}
