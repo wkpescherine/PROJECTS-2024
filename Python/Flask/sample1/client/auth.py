7ad3636b7e5cd240c7e3f899210f00b7d989ebe4
William Pescherine <wkpescherine@yahoo.com>
2024-09-03 22:34:17 -0400

    9/3/24     Python         Flask             sample1                 Continued expansion of project.                 Added more views

--- Python/Flask/sample1/client/auth.py
+++ Python/Flask/sample1/client/auth.py
@@ -3,3 +3,15 @@
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logged out</p>"
+
+@auth.route('/sign-up')
+def signup():
+    return "<p>Sign-In</p>"
